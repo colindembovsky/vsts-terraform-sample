8c626d7a91da1c7ac13cb39e2ee06daf168c894d
Colin Dembovsky <colindembovsky@gmail.com>
2018-07-30 14:02:04 -0700

    Add download script, move scripts to scripts folder

---
+++ scripts/download-terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f $1/terraform ]; then
+  echo "Downloading terraform from ${2} to ${1}"
+  curl $2 -o terraform.zip
+  mkdir -p $1
+  unzip terraform.zip -d $1
+else
+  echo "Terraform already exists in ${1}"
+fi
+$1/terraform -version
