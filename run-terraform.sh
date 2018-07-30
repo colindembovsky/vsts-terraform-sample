d1ed2177f0b666d915d9bec5ecf627106aa88045
Colin Dembovsky <colindembovsky@gmail.com>
2018-07-30 13:39:21 -0700

    Update args

--- run-terraform.sh
+++ run-terraform.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "*********** Initialize backend"
-echo 'access_key = "$(ARM_ACCESS_KEY)"' > ../backend.tfvars
-$(TerraformPath)/terraform init -backend-config=../backend.tfvars -no-color
+echo 'access_key = "$1"' > ../backend.tfvars
+$2/terraform init -backend-config=../backend.tfvars -no-color
 
 echo "*********** Create or select workspace"
-if [ $($(TerraformPath)/terraform workspace list | grep $(Release.EnvironmentName) | wc -l) -eq 0 ]; then
-  echo "Create new workspace $(Release.EnvironmentName)"
-  $(TerraformPath)/terraform workspace new $(Release.EnvironmentName) -no-color
+if [ $($2/terraform workspace list | grep $3 | wc -l) -eq 0 ]; then
+  echo "Create new workspace $3"
+  $2/terraform workspace new $3 -no-color
 else
-  echo "Switch to workspace $(Release.EnvironmentName)"
-  $(TerraformPath)/terraform workspace select $(Release.EnvironmentName) -no-color
+  echo "Switch to workspace $3"
+  $2/terraform workspace select $3 -no-color
 fi
 
 echo "*********** Run 'plan'"
-$(TerraformPath)/terraform plan --var-file=../global.tfvars --var-file=../release.tfvars -var="release=$(Release.ReleaseName)" --out=./tf.plan -no-color -input=false
+$2/terraform plan --var-file=../global.tfvars --var-file=../release.tfvars -var="release=$4" --out=./tf.plan -no-color -input=false
 
 echo "*********** Run 'apply'"
-# $(TerraformPath)/terraform apply -no-color -input=false -auto-approve ./tf.plan
+# $2/terraform apply -no-color -input=false -auto-approve ./tf.plan
